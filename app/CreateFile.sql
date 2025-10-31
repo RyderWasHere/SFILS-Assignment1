bb3cffce836f1f45fef6f705d1d2c8df9dd7f4a6
RyderWasHere <redyrfell@gmail.com>
2025-10-31 12:08:40 -0800

    program now connects to db. Writting script to build the DB

---
+++ app/CreateFile.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE RfAssignment1;
+
+CREATE TABLE PatronTypes(
+PatronTypeID int Primary KEY,
+PatronTypeDefinition varchar(225) NOT NULL,
+PRIMARY KEY(PatronTypeID)
+);
+
+CREATE TABLE HomeLibararyCodes(
+HomeLibraryCode varchar(2) NOT NULL,
+HomeLibraryDefinition varchar(225),
+PRIMARY KEY(HomeLibraryCode)
+);
+CREATE TABLE Patrons (
+PatronID int auto_increment, 
+PatronTypeID int NOT NULL,
+TotalCheckouts int,
+TotalRenews int,
+AgeRangeLow int,
+AgeRangeHigh int,
+HomeLibraryCode varchar(2),
+CirculationActiveMonth varchar(225),
+CirculationActiveYear int,
+NotificationPreferenceCode char,
+providedEmailAddress bool,
+WithinSanFranciscoCounty bool,
+PatronRegisterYear int,
+PRIMARY KEY (PatronID),
+FOREIGN KEY(PatronTypeID) references PatronTypes(PatronTypeID),
+FOREIGN KEY(HomeLibraryCode) references HomeLibararyCodes(HomeLibraryCode)
+);
+insert into PatronTypes (PatronTypeID, PatronTypeDefinition)
+values 
+	(0, 'Adult'),
+	(1, 'Juvenile'),
+	(2, 'Teen'),
+    (3, 'Senior'),
+    (4, 'Welcome'),
+    (16, 'Digital Access Card');
+/*
+insert into HomeLibararyCodes(HomeLibraryCode, HomeLibraryDefinition)
+values
+	('r3', 'RichMond'),
+    ('x', 'Main'),
+    ('m4', 'Merced'),
+    ('b2', 'Bayview'),
+    ('s7', 'Sunset'),
+    ('m8', 'Mission Bay'), 
+    ('p3', 'Parkside');
+    */
+    
--- app/SQl_Connector_app.py
+++ app/SQl_Connector_app.py
@@ -1,14 +1,21 @@
+from sqlite3 import Cursor
 import mysql.connector
 
 try:
     mydb = mysql.connector.connect(
         host="localhost",
-        user="your_username",
-        password="your_password",
-        database="your_database_name" # Optional: specify a database to connect to
+        user="root",
+        password="admin",
+        database="RFAssignment1" # Optional: specify a database to connect to
     )
     print("Connected to MySQL database!")
-
 except mysql.connector.Error as err:
     print(f"Error: {err}")
 
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for db in mycursor:
+    print(db)
