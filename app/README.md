22cfa3c2aba1d9473852c14ba1a37a6edd5f2470
RyderWasHere <redyrfell@gmail.com>
2025-10-31 13:34:56 -0800

    connected document to the program

--- app/README.md
+++ app/README.md
@@ -1,3 +1,7 @@
 # The App to Query, Retrieve, and Update Database
 
 This folder contains the source code of the (minimal) application that allows user interaction with the database.
+#Libaries needed to run:
+Pandas
+mysql Connector
+openpyxl
--- app/SQl_Connector_app.py
+++ app/SQl_Connector_app.py
@@ -1,20 +1,34 @@
+from asyncio.windows_events import NULL
+import os
 from sqlite3 import Cursor
+from tkinter import CHAR
+from xml.dom import UserDataHandler
 import mysql.connector
 import pandas as pd
+
 class DBEntry:
-    def __init__(self, PatronTypeID):
-        self.PatronTypeID = PatronTypeID
-        self.TotalCheckots = TotalCheckouts
-        self.TotalRenews 
-        self.AgeRangeLow
-        self.AgeRangehigh
-        self.HomeLibraryCode
-        self.CirculationActiveMonth
-        self.CirculationActiveYear
-        self.NotificationPreferenceCode
-        self.providedEmailAddress
-        self.WithinSanFranciscoCounty
-        self.PatronRegisterYear
+    def __init__(self):
+        self.PatronTypeID = None
+        self.TotalCheckots = None
+        self.TotalRenews = None
+        self.AgeRangeLow = None
+        self.AgeRangehigh = None
+        self.HomeLibraryCode = None
+        self.NotificationPreferenceCode = None
+        self.CirculationActiveMonth = None
+        self.CirculationActiveYear = None
+        self.providedEmailAddress = None
+        self.WithinSanFranciscoCounty = None
+        self.PatronRegisterYear = None
+
+def WritePatronType(mycursor):
+    return
+
+def WriteLibraryCode(mycursor):
+    return
+def WritePatron(mycursor):
+    return
+
 try:
     mydb = mysql.connector.connect(
         host="localhost",
@@ -22,21 +36,28 @@ try:
         password="admin",
         database="RFAssignment1" # Optional: specify a database to connect to
     )
-    print("Connected to MySQL database!")
+    print("Connected to RFAssignment1 database!")
 except mysql.connector.Error as err:
     print(f"Error: {err}")
-def WritePatronType(mycursor):
-    return
-def WriteLibraryCode(mucursor):
-    return
 
-#opening file to be read
-df = pd.read_excel('Sample.xlsx', sheet_name='Sheet1')
 
+print("1 - import Sample.xlsx file\n2 - query existing database")
+userinput = input()
+
+if int(userinput) == 1:
+    print("opening Sample.xlsn file...")
+    #opening file to be read
+    try:
+        df = pd.read_excel('app/Sample.xlsx', sheet_name='Sheet1')
+        
+    except:
+        print("failed to open file")
+    #go through all the lines in the DB
+    for index, row in df.iterrows():
+
+        print(index)
 mycursor = mydb.cursor()
 
 mycursor.execute("SHOW TABLES")
-
-
 for db in mycursor:
     print(db)
