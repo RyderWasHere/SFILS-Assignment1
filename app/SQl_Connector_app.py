7184f061392d6720891c59602d0e82af3481268c
RyderWasHere <redyrfell@gmail.com>
2025-11-01 14:36:31 -0800

    makes the class sucessfully

--- app/SQl_Connector_app.py
+++ app/SQl_Connector_app.py
@@ -1,4 +1,6 @@
 from asyncio.windows_events import NULL
+import math
+from math import isnan, nan
 import os
 from sqlite3 import Cursor
 from tkinter import CHAR
@@ -61,27 +63,35 @@ if int(userinput) == 1:
     #go through all the lines in the DB
     for index, row in df.iterrows():
         NewRow = DBEntry()
-        NewRow.PatronTypeID = row['ColumnA']
-        NewRow.PatronTypeDefinition = row['ColumnB']
-        NewRow.TotalCheckots = row['ColumnC']
-        NewRow.TotalRenews = row['ColumnD']
+        NewRow.PatronTypeID = row['Patron Type Code\npatron_type_code']
+        NewRow.PatronTypeDefinition = row['Patron Type Definition\n']
+        NewRow.TotalCheckots = row['Total Checkouts\ncheckout_total']
+        NewRow.TotalRenews = row['Total Renewals\nrenewal_total']
 
         #get low and high age
-        combinedAge = row['ColumnE']
-        separatedAge = re.findall(r'\d+', combinedAge)
-        low, high = map(int, separatedAge)
-        NewRow.AgeRangeLow = low
-        NewRow.AgeRangehigh = high
-
-        NewRow.HomeLibraryCode = row['ColumnF']
-        NewRow.HomeLibraryDefinition = row['ColumnG']
-        NewRow.CirculationActiveMonth = row['ColumnH']
-        NewRow.CirculationActiveYear = row['ColumnI']
-        NewRow.NotificationPreferenceCode = row['ColumnJ']
-        NewRow.NotificationCodeDefinition = row['ColumnK']
-        NewRow.providedEmailAddress = row['ColumnL']
-        NewRow.WithinSanFranciscoCounty = row['ColumnM']
-        NewRow.PatronRegisterYear = row['ColumnN']
+        combinedAge = row['Age Range\n']
+        #not the best way to handle this case but it works
+        try:
+            separatedAge = re.findall(r'\d+', combinedAge)
+            try:
+                low, high = map(int, separatedAge)
+                NewRow.AgeRangeLow = low
+                NewRow.AgeRangehigh = high
+            except:
+                NewRow.AgeRangeLow = separatedAge
+                NewRow.AgeRangehigh = None
+        except:
+            NewRow.AgeRangeLow = None
+            NewRow.AgeRangehigh = None
+        NewRow.HomeLibraryCode = row['Home Library Code\nhome_library_code']
+        NewRow.HomeLibraryDefinition = row['Home Library Definition\n']
+        NewRow.CirculationActiveMonth = row['Circulation Active Month\n']
+        NewRow.CirculationActiveYear = row['Circulation Active Year\n']
+        NewRow.NotificationPreferenceCode = row['Notification Preference Code\nnotification_medium_code']
+        NewRow.NotificationCodeDefinition = row['Notification Code Definition']
+        NewRow.providedEmailAddress = row['Provided Email Address\n']
+        NewRow.WithinSanFranciscoCounty = row['Within San Francisco County']
+        NewRow.PatronRegisterYear = row['Year Patron Registered']
         print(index)
 
 
